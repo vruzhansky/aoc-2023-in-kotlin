4cadca569c176ac6532d2f5d43aaee9917d5b9da
Volodymyr Ruzhanskyi <vruzhansky@gmail.com>
2023-12-02 13:47:39 +0100

    day 2

---
+++ src/Day02.kt
@@ -0,0 +1,52 @@
+fun main() {
+    data class Round(val r: Int, val g: Int, val b: Int) {
+        fun possible(target: Round) =
+            (r <= target.r && g <= target.g && b <= target.b)
+    }
+
+    data class Game(val id: Int, val rounds: List<Round>)
+
+    fun parse(str: String): Game {
+        val (gameStr, roundsStr) = str.split(": ")
+        val gameId = gameStr.replace("Game ", "").toInt()
+        val rawRounds = roundsStr.split("; ")
+        val rounds = rawRounds.map { roundStr ->
+            var (r, g, b) = Triple(0, 0,0)
+            roundStr.split(", ").forEach {
+                when {
+                    it.endsWith(" red") -> r = it.replace(" red", "").toInt()
+                    it.endsWith(" green") -> g = it.replace(" green", "").toInt()
+                    it.endsWith(" blue") -> b = it.replace(" blue", "").toInt()
+                }
+            }
+            Round(r, g, b)
+        }
+        return Game(gameId, rounds)
+    }
+
+    val target = Round(12, 13, 14)
+
+    fun part1(input: List<String>): Int {
+        return input.sumOf { str ->
+            val game = parse(str)
+            if (game.rounds.all { it.possible(target) }) game.id else 0
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.sumOf { str ->
+            val game = parse(str)
+            val rounds = game.rounds
+            rounds.maxOf { it.r } * rounds.maxOf { it.g } * rounds.maxOf { it.b }
+        }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
+
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
