e18aaf523fcbd87aa415ff5555e6266f92fd982b
Volodymyr Ruzhanskyi <vruzhansky@gmail.com>
2023-12-08 14:23:00 +0100

    Day 8

---
+++ src/Day08.kt
@@ -0,0 +1,71 @@
+private const val DAY = "08"
+
+
+fun main() {
+    data class Nav(val instr: String, val nodes: Map<String, Pair<String, String>>)
+
+    fun parse(input: List<String>): Nav {
+        val instr = input.first()
+        val nodes = input.drop(2).associate {
+            val (node, nextNodes) = it.split(" = ")
+            val (left, right) = nextNodes.removePrefix("(").removeSuffix(")").split(", ")
+            node to (left to right)
+        }
+        return Nav(instr, nodes)
+    }
+
+    fun steps(nav: Nav, from: String, toPredicate: (String) -> Boolean): Int {
+        val (instr, nodes) = nav
+        var steps = 0
+        var nextInstrIdx = 0
+        var cur = from
+
+        while (!toPredicate(cur)) {
+            val node = nodes[cur] ?: return -1
+            cur = if (instr[nextInstrIdx] == 'L') node.first else node.second
+            steps++
+            nextInstrIdx = if (nextInstrIdx >= instr.length - 1) 0 else nextInstrIdx + 1
+        }
+
+        return steps
+    }
+
+    fun lcm(n1: Long, n2: Long): Long {
+        var i = 1
+        var gcd = 1
+        while (i <= n1 && i <= n2) {
+            if (n1 % i == 0L && n2 % i == 0L) gcd = i
+            i++
+        }
+
+        return n1 * n2 / gcd
+    }
+
+    fun part1(input: List<String>): Int {
+        val nav = parse(input)
+
+        return steps(nav, "AAA") { it == "ZZZ" }
+    }
+
+    fun part2(input: List<String>): Long {
+        val nav = parse(input)
+
+        val starts = nav.nodes.keys.filter { it.endsWith("A") }.toList()
+
+        return starts.map { start ->
+            steps(nav, start) { it.endsWith("Z") }
+        }
+            .map { it.toLong() }
+            .reduce { acc, i -> lcm(acc, i) }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day${DAY}_test")
+    val testInput2 = readInput("Day${DAY}_2_test")
+    check(part1(testInput).also { println(it) } == 2)
+    check(part2(testInput2).also { println(it) } == 6L)
+
+    val input = readInput("Day$DAY")
+    check(part1(input).also { println(it) } == 20569)
+    check(part2(input).also { println(it) } == 21366921060721L)
+}
