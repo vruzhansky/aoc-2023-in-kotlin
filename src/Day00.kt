880e2327ebed75dc2d9ac3cdbca6c69736f10eab
Volodymyr Ruzhanskyi <vruzhansky@gmail.com>
2023-12-16 12:42:57 +0100

    Day 00

---
+++ src/Day00.kt
@@ -0,0 +1,25 @@
+import java.util.*
+
+private const val DAY = "16"
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+
+        return 0
+    }
+
+// test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day${DAY}_test")
+    check(part1(testInput).also { println(it) } == 0)
+    check(part2(testInput).also { println(it) } == 0)
+
+    val input = readInput("Day$DAY")
+    check(part1(input).also { println(it) } == 0)
+    check(part2(input).also { println(it) } == 0)
+}
